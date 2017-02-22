1c9708d7a0c855651f9786f91cadb2efde9ea301
elison.esouza <elison.esouza@CASX5X5X5W0736.senacsp.edu.br>
2017-02-21 21:30:42 -0300

    Mostra se for impar

--- testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
@@ -16,7 +16,7 @@ public class Principal {
      */
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            if(i % 2 == 0){
+            if(i % 1 == 0){
                   System.out.println("Hello Word! " + (i+1) + " º");   
 
             }
